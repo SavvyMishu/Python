640d649151aea732f18e9766f84deace3ceb1ec1
Tithi Bala <tithibala2024@gmail.com>
2025-02-01 00:42:01 +0600

    add intro.py

---
+++ intro.py
@@ -0,0 +1,22 @@
+# print("20+20")
+
+# name = "tithi"
+# age = 22
+# grade = 3.04
+
+# print("my name is = " , name , "i am " , age , "years old" , "and my grade is " , grade)
+
+# print(type(name))
+# print(type(age))
+# print(type(grade))
+
+# ---------------type casting-------------
+# a = int("2")
+# b = 8.98
+
+# print(a + b)
+# print(type(a))
+
+# -----how to take input------------------
+a = input( )
+print("my name is " , a)
